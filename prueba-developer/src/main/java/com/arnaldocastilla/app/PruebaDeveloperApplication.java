8dba347147ec96f382ed6e47758085c9976d1211
al2rex <arnaldo.castilla@gmail.com>
2020-11-21 01:57:20 -0500

    first commit

---
+++ prueba-developer/src/main/java/com/arnaldocastilla/app/PruebaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.arnaldocastilla.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDeveloperApplication.class, args);
+	}
+
+}
