568a45896783ca4718a229c0db811cb58d728b3d
al2rex <arnaldo.castilla@gmail.com>
2020-11-21 04:55:52 -0500

    savando cambios.

--- prueba-developer/src/main/java/com/arnaldocastilla/app/services/EmpleadoImpl.java
+++ prueba-developer/src/main/java/com/arnaldocastilla/app/services/EmpleadoImpl.java
@@ -10,15 +10,16 @@ import java.util.Date;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmpleadoImpl implements IEmpleado{
+public class EmpleadoImpl implements IEmpleado {
 
 	@Override
 	public Boolean validarCamposNull(String nombre, String apellido, String tipodedocumento, String numerodedocumento,
 			String fechadenacimiento, String fechadevinculacion, String cargo, Double salario) {
-		if(nombre.isEmpty() || apellido.isEmpty() || tipodedocumento.isEmpty() || numerodedocumento.isEmpty() || fechadenacimiento.isEmpty() ||  fechadevinculacion.isEmpty() || cargo.isEmpty()) {
+		if (nombre.isEmpty() || apellido.isEmpty() || tipodedocumento.isEmpty() || numerodedocumento.isEmpty()
+				|| fechadenacimiento.isEmpty() || fechadevinculacion.isEmpty() || cargo.isEmpty()) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -32,7 +33,7 @@ public class EmpleadoImpl implements IEmpleado{
 	public Boolean empleadoMayorEdad(LocalDate fechaNacimiento) {
 		Period edad = Period.between(fechaNacimiento, LocalDate.now());
 
-		if(edad.getYears() >= 18) {
+		if (edad.getYears() >= 18) {
 			return true;
 		}
 		return false;
@@ -41,8 +42,7 @@ public class EmpleadoImpl implements IEmpleado{
 	@Override
 	public LocalDate convertirLocalDate(String fecha) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		  LocalDate localDate = LocalDate.parse(fecha, formatter);
-
+		LocalDate localDate = LocalDate.parse(fecha, formatter);
 		return localDate;
 	}
 
